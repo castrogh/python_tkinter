0200e4d51b3fddbf5fdadc375ae6781f0a47e83f
Gabriel Castro <ghscastro@hotmail.com>
2023-04-03 23:47:00 -0300

    Modificando o parâmetro text da variável lbl_cotacoes.

--- estudo_tkinter.py
+++ estudo_tkinter.py
@@ -15,23 +15,21 @@ def pegar_cotacoes():
     Euro: {cotacao_euro}
     BTC: {cotacao_btc}'''
 
-    print(texto)
-
-pegar_cotacoes()
+    lbl_cotacoes["text"] = texto #ao utlizar o ["text"], estou modificando o parâmetro text da variavel lbl_cotacoes para o conteúdo da variável texto
 
 
 janela = Tk()
 
 janela.title("Cotações - US$, EUR e BTC")
 
-txt_clique = Label(janela, text="Clique no botão abaixo para ver as cotações")
-txt_clique.grid(column=0, row=0)
+lbl_clique_btn = Label(janela, text="Clique no botão abaixo para ver as cotações")
+lbl_clique_btn.grid(column=0, row=0)
 
 btn_cotacao = Button(janela, text="Clique Aqui!", command = pegar_cotacoes) #ao passar a função pegar_cotacoes, como um parâmetro, sem os (), estou informando que ela só deve ser executada quando o botão for clicado, caso fosse passada com os (), a função seria executada automaticamente
 btn_cotacao.grid(column=0, row=1)
 
-txt_cotacoes = Label(janela, text="As cotações serão mostradas aqui")
-txt_cotacoes.grid(column=0, row=2)
+lbl_cotacoes = Label(janela, text="As cotações serão mostradas aqui")
+lbl_cotacoes.grid(column=0, row=2)
 
 
 janela.mainloop()
